2f0014099144760451ec63cd5b17e81e8ecdba7e
Cassidy <cassidy0kovell@gmail.com>
2024-03-05 12:05:21 -0800

    added a validate function for the amount of characters a user can enter and added tests for the circle

--- index.js
+++ index.js
@@ -1,67 +1,74 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const { Triangle, Circle, Square } = require('./lib/shapes');
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Triangle, Circle, Square } = require("./lib/shapes");
 
 async function promptUser() {
-    const userInput = await inquirer.prompt([
-      {
-        type: "input",
-        name: "characters",
-        message:
-          "Enter up to three characters that will be displayed on the logo.",
-      },
-      {
-        type: "input",
-        name: "textColor",
-        message: "What color would you like the text to be?",
-      },
-      {
-        type: "list",
-        name: "shape",
-        message: "Select the shape you want:",
-        choices: ["Circle", "Triangle", "Square"],
-      },
-      {
-        type: "input",
-        name: "shapeColor",
-        message: "What color would you like the shape to be?",
-      },
-    ]);
-    return userInput;
+  const userInput = await inquirer.prompt([
+    {
+      type: "input",
+      name: "characters",
+      message: "Enter up to three characters that will be displayed on the logo.",
+      validate: function(characters) {
+          if (characters.length > 0 && characters.length <= 3) {
+              return true;
+          } else if (characters.length === 0) {
+              return "Please enter at least one character.";
+          } else {
+              return "Please enter up to three characters only.";
+          }
+      }
+  },
+    {
+      type: "input",
+      name: "textColor",
+      message: "What color would you like the text to be?",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Select the shape you want:",
+      choices: ["Circle", "Triangle", "Square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "What color would you like the shape to be?",
+    },
+  ]);
+  return userInput;
+}
+
+function generateLogo(userInput) {
+  let shape;
+  switch (userInput.shape) {
+    case "Triangle":
+      shape = new Triangle();
+      break;
+    case "Circle":
+      shape = new Circle();
+      break;
+    case "Square":
+      shape = new Square();
+      break;
+    default:
+      console.log("Invalid shape selected.");
+      return;
   }
 
-  function generateLogo(userInput) {
-    let shape;
-    switch (userInput.shape) {
-        case 'Triangle':
-            shape = new Triangle();
-            break;
-        case 'Circle':
-            shape = new Circle();
-            break;
-        case 'Square':
-            shape = new Square();
-            break;
-        default:
-            console.log('Invalid shape selected.');
-            return;
-    }
+  shape.setShapeColor(userInput.shapeColor);
+  shape.setText(userInput.characters);
+  shape.setColor(userInput.textColor);
 
-    shape.setShapeColor(userInput.shapeColor);
-    shape.setText(userInput.characters);
-    shape.setColor(userInput.textColor);
-    
-    const svgContent = shape.render();
+  const svgContent = shape.render();
 
-    fs.writeFileSync('logo.svg', svgContent);
+  fs.writeFileSync("logo.svg", svgContent);
 
-    console.log('Generated logo.svg');
+  console.log("Generated logo.svg");
 }
 
 async function run() {
-    const userInput = await promptUser();
-    generateLogo(userInput);
+  const userInput = await promptUser();
+  generateLogo(userInput);
 }
 
 run();
